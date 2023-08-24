f7328cb7b1424007b398e0e7a563d0b678e6c174
px0train <143118916+px0train@users.noreply.github.com>
2023-08-25 04:10:52 +0800

    Update px0train.py

--- px0train.py
+++ px0train.py
@@ -2,12 +2,13 @@ import argparse
 import os
 import requests
 
-def download_file(url):
+def download_file(url, path):
     response = requests.get(url)
     filename = url.split('/')[-1]
-    with open(filename, 'wb') as f:
+    filepath = os.path.join(path, filename)
+    with open(filepath, 'wb') as f:
         f.write(response.content)
-    return filename
+    return filepath
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,14 +16,15 @@ def main():
     parser.add_argument('--password', required=True, help='自定义内容')
     parser.add_argument('--train', required=True, help='自定义内容')
     parser.add_argument('--url', required=True, help='文件下载URL')
+    parser.add_argument('--path', required=True, help='下载文件的路径')
     args = parser.parse_args()
 
-    lc0_main_filename = download_file(args.url)
+    lc0_main_filepath = download_file(args.url, args.path)
 
-    os.chmod(lc0_main_filename, 0o755)
+    os.chmod(lc0_main_filepath, 0o755)
 
     train_only = args.train
-    command = f'./{lc0_main_filename} --user={args.user} --password={args.password} --train-only={train_only}'
+    command = f'{lc0_main_filepath} --user={args.user} --password={args.password} --train-only={train_only}'
     os.system(command)
 
 if __name__ == '__main__':
